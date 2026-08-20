e5af5299c0579766210637017b0bb17a6e097ed3
Tara Kaos <tara.kaos@example.com>
2026-08-20 14:13:03 -0500

    Added Odin Recipe Book to projects.js

--- data/projects.js
+++ data/projects.js
@@ -4,5 +4,11 @@ const projects = [
     description: "Move the robot with WASD, Spacebar to jump, and shift to run. Can you collect all the stars?",
     tech: ["Unity"],
     link: "https://play.unity.com/en/games/28edfc19-064e-4ecd-b8c7-1ae577f1593a/tara-web-build"
+  },
+  {
+    title: "Odin Recipe Book",
+    description: "Simple recipe book with 3 recipes (Gumbo, Steak, and Chicken Noodle Soup). This project was built as part of the Odin Project",
+    tech: ["HTML"],
+    link: "https://tarakaos.github.io/odin-recipes/"
   }
 ];
